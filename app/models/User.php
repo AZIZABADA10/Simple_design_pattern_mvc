2a0ec67c4964776462341216873cf821361c518f
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-13 14:04:30 +0000

    Add User model with sample data

---
+++ app/models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+class User
+{
+    public function getAllUsers()
+    {
+        return [
+            ['id' => 1, 'name' => 'Aziz'],
+            ['id' => 2, 'name' => 'Sara'],
+            ['id' => 3, 'name' => 'Youssef'],
+        ];
+    }
+}
