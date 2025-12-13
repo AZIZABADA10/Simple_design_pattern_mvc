10e66dd6e88b1efb216a323978a7aac68866885c
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-13 14:49:51 +0000

    Create User model with register and login methods

--- app/models/User.php
+++ app/models/User.php
@@ -1,21 +1,32 @@
 <?php
 require_once __DIR__ . '/../../config/database.php';
 
+
 class User
 {
-    private $conn;
+private $conn;
 
-    public function __construct()
-    {
-        $this->conn = Database::connect();
-    }
 
-    public function getAllUsers()
-    {
-        $sql = "SELECT * FROM users";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute();
+public function __construct()
+{
+$this->conn = Database::connect();
+}
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+
+public function register($name, $email, $password)
+{
+$hash = password_hash($password, PASSWORD_DEFAULT);
+$sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+$stmt = $this->conn->prepare($sql);
+return $stmt->execute([$name, $email, $hash]);
+}
+
+
+public function login($email)
+{
+$sql = "SELECT * FROM users WHERE email = ?";
+$stmt = $this->conn->prepare($sql);
+$stmt->execute([$email]);
+return $stmt->fetch(PDO::FETCH_ASSOC);
+}
 }
