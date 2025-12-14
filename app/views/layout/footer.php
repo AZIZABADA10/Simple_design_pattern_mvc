19ca7cbeaf7660be95da682c24cf88a9e003be35
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-14 17:52:27 +0000

    style(layout): améliorer le footer avec un design moderne

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,6 +1,6 @@
-<hr>
-<footer>
-    <p>&copy; 2025 - MVC Simple</p>
+<footer class="bg-gray-800 text-gray-300 py-4 text-center">
+    <p>&copy; 2025 - MVC Simple | Tous droits réservés</p>
 </footer>
+
 </body>
 </html>
